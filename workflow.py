ab712f43aa0ca3e6d870be9ee0786017f833fe6a
wizensoft <annguk@koreablaster.localdomain>
2020-08-12 14:57:13 +0900

    deploy

--- workflow.py
+++ workflow.py
@@ -9,11 +9,11 @@ from airflow.models import Variable
 # bar = Variable.get("bar", deserialize_json=True)
 # baz = Variable.get("baz", default_var=None)
 from airflow.operators.bash_operator import BashOperator
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
+# from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.mysql_hook import MySqlHook
 from airflow.operators.mysql_operator import MySqlOperator
-# from airflow.operators import WorkflowSensor
+from airflow.operators import WorkflowSensor
 from datetime import datetime, timedelta
 from airflow.utils.helpers import chain
 
@@ -676,7 +676,7 @@ def get_status_00(**context):
 
 with models.DAG("workflow", default_args=default_args, schedule_interval=timedelta(minutes=1)) as dag:
     # Watch workflow process
-    # wf_sensor = WorkflowSensor(task_id='wf_sensor_task', poke_interval=3, dag=dag)
+    wf_sensor = WorkflowSensor(task_id='wf_sensor_task', poke_interval=3, dag=dag)
     # Start workflow    
     wf_start = PythonOperator(task_id=WORKFLOW_START_TASK, python_callable=get_workflow, provide_context=True, dag=dag)
     # Status get
@@ -747,8 +747,10 @@ with models.DAG("workflow", default_args=default_args, schedule_interval=timedel
     # Complete: 완료처리
     complete = BashOperator(task_id='complete_task', bash_command='echo get complete 완료처리', dag=dag)
 
+
+    ##
     # Workflow Start
-    wf_start >> instances >> settings >> status
+    wf_sensor >> wf_start >> instances >> settings >> status
     # 결재중이 아니면 완료 처리
     instances >> complete
     
