a2595e9ccdf48450c3d82cc6e474941c537aa540
wizensoft <wizensoft@hotmail.com>
2020-08-18 11:27:45 +0900

    deploy

--- workflow.py
+++ workflow.py
@@ -69,61 +69,61 @@ def get_workflow(**context):
     cipher  = Fernet("5EpGKTgEhjBn6cX67I20u0p2gUFznUAEbKYAh0ghlPw=")
     cipher_text = cipher_suite.encrypt(b"dnlwps1!")
     logging.info(f'cipher_text: {cipher_text}')
-    
-    db = MySqlHook(mysql_conn_id='mariadb', schema="djob")
-    # wfp = context['ti'].xcom_pull(task_ids='wf_sensor_task', key=WORKFLOW_PROCESS)
-    # if wfp:
-    #     for row in wfp:
-    #         logging.info(f'wfp row {row}')
-    # else:
-    #     logging.info(f'WORKFLOW_PROCESS data is empty')
 
-    sql = """
-    select
-        workflow_process_id,ngen,site_id,application_id,instance_id,schema_id,name,workflow_instance_id,state,retry_count,ready,
-        execute_date,created_date,bookmark,version,request,reserved,message
-    from
-        workflow_process
-    where 
-        ready > 0 and retry_count < 10
-    limit 1
-    """
-    task = {}
-    rows = db.get_records(sql)
-    for row in rows:
-        model = {
-            'workflow_process_id':row[0]
-            # 'ngen':row[1],
-            # 'site_id':row[2],
-            # 'application_id':row[3],
-            # 'instance_id':row[4],
-            # 'schema_id':row[5],
-            # 'name':row[6],
-            # 'workflow_instance_id':row[7],
-            # 'state':row[8],
-            # 'retry_count':row[9],
-            # 'ready':row[10],
-            # 'execute_date':str(row[11]),
-            # 'created_date':str(row[12]),
-            # 'bookmark':row[13],
-            # 'version':row[14],
-            # 'request':row[15],
-            # 'reserved':row[16],
-            # 'message':row[17]
-        }
-        task = model
+    # db = MySqlHook(mysql_conn_id='mariadb', schema="djob")
+    # # wfp = context['ti'].xcom_pull(task_ids='wf_sensor_task', key=WORKFLOW_PROCESS)
+    # # if wfp:
+    # #     for row in wfp:
+    # #         logging.info(f'wfp row {row}')
+    # # else:
+    # #     logging.info(f'WORKFLOW_PROCESS data is empty')
 
-    # 객체가 있는 경우 처리
-    if task:
-        context['ti'].xcom_push(key=WORKFLOWS, value=task)
-        sql = f"""
-        update workflow_process
-            set ready = 0, bookmark = 'start'
-        where workflow_process_id = %s
-        """
-        db.run(sql, autocommit=True, parameters=[task['workflow_process_id']])
+    # sql = """
+    # select
+    #     workflow_process_id,ngen,site_id,application_id,instance_id,schema_id,name,workflow_instance_id,state,retry_count,ready,
+    #     execute_date,created_date,bookmark,version,request,reserved,message
+    # from
+    #     workflow_process
+    # where 
+    #     ready > 0 and retry_count < 10
+    # limit 1
+    # """
+    # task = {}
+    # rows = db.get_records(sql)
+    # for row in rows:
+    #     model = {
+    #         'workflow_process_id':row[0]
+    #         # 'ngen':row[1],
+    #         # 'site_id':row[2],
+    #         # 'application_id':row[3],
+    #         # 'instance_id':row[4],
+    #         # 'schema_id':row[5],
+    #         # 'name':row[6],
+    #         # 'workflow_instance_id':row[7],
+    #         # 'state':row[8],
+    #         # 'retry_count':row[9],
+    #         # 'ready':row[10],
+    #         # 'execute_date':str(row[11]),
+    #         # 'created_date':str(row[12]),
+    #         # 'bookmark':row[13],
+    #         # 'version':row[14],
+    #         # 'request':row[15],
+    #         # 'reserved':row[16],
+    #         # 'message':row[17]
+    #     }
+    #     task = model
 
-    return task
+    # # 객체가 있는 경우 처리
+    # if task:
+    #     context['ti'].xcom_push(key=WORKFLOWS, value=task)
+    #     sql = f"""
+    #     update workflow_process
+    #         set ready = 0, bookmark = 'start'
+    #     where workflow_process_id = %s
+    #     """
+    #     db.run(sql, autocommit=True, parameters=[task['workflow_process_id']])
+
+    # return task
     
 def start_workflow():
     db = MySqlHook(mysql_conn_id='mariadb', schema="djob")
